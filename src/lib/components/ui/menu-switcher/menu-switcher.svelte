2948bb97d2930bb59b145f383aedc5d131a23c92
Leninbevan <charleslenin431@gmail.com>
2025-03-14 12:14:37 +0530

    added avatar

--- src/lib/components/ui/menu-switcher/menu-switcher.svelte
+++ src/lib/components/ui/menu-switcher/menu-switcher.svelte
@@ -1,48 +1,56 @@
 <script lang="ts">
-	import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
-	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
-	import Check from "@lucide/svelte/icons/check";
-	import ChevronsUpDown from "@lucide/svelte/icons/chevrons-up-down";
-	import GalleryVerticalEnd from "@lucide/svelte/icons/gallery-vertical-end";
+  import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
+  import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+  import Check from "@lucide/svelte/icons/check";
+  import ChevronsUpDown from "@lucide/svelte/icons/chevrons-up-down";
+  import GalleryVerticalEnd from "@lucide/svelte/icons/gallery-vertical-end";
+  import Avatar from "../avatar/avatar.svelte";
+  import AvatarImage from "../avatar/avatar-image.svelte";
+  import AvatarFallback from "../avatar/avatar-fallback.svelte";
 
-	let { versions, defaultVersion }: { versions: string[]; defaultVersion: string } = $props();
+  let {
+    versions,
+    defaultVersion,
+  }: { versions: string[]; defaultVersion: string } = $props();
 
-	let selectedVersion = $state(defaultVersion);
+  let selectedVersion = $state(defaultVersion);
 </script>
 
 <Sidebar.Menu>
-	<Sidebar.MenuItem>
-		<DropdownMenu.Root>
-			<DropdownMenu.Trigger>
-				{#snippet child({ props })}
-					<Sidebar.MenuButton
-						size="lg"
-						class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-						{...props}
-					>
-						<div
-							class="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg"
-						>
-							<img src="https://github.com/shadcn.png" class="rounded" alt="">
-						</div>
-						<div class="flex flex-col gap-0.5 leading-none">
-							<span class="font-semibold">Lenin Bevan</span>
-							<span class="">leninbevan@gmail.com</span>
-						</div>
-						<ChevronsUpDown class="ml-auto" />
-					</Sidebar.MenuButton>
-				{/snippet}
-			</DropdownMenu.Trigger>
-			<DropdownMenu.Content class="w-[var(--bits-dropdown-menu-anchor-width)]" align="start">
-				{#each versions as version (version)}
-					<DropdownMenu.Item onSelect={() => (selectedVersion = version)}>
-						v{version}
-						{#if version === selectedVersion}
-							<Check class="ml-auto" />
-						{/if}
-					</DropdownMenu.Item>
-				{/each}
-			</DropdownMenu.Content>
-		</DropdownMenu.Root>
-	</Sidebar.MenuItem>
+  <Sidebar.MenuItem>
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        {#snippet child({ props })}
+          <Sidebar.MenuButton
+            size="lg"
+            class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+            {...props}
+          >
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" alt="chatbot" />
+              <AvatarFallback>chatbot</AvatarFallback>
+            </Avatar>
+            <div class="flex flex-col gap-0.5 leading-none">
+              <span class="font-semibold">Lenin Bevan</span>
+              <span class="">leninbevan@gmail.com</span>
+            </div>
+            <ChevronsUpDown class="ml-auto" />
+          </Sidebar.MenuButton>
+        {/snippet}
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Content
+        class="w-[var(--bits-dropdown-menu-anchor-width)]"
+        align="start"
+      >
+        {#each versions as version (version)}
+          <DropdownMenu.Item onSelect={() => (selectedVersion = version)}>
+            v{version}
+            {#if version === selectedVersion}
+              <Check class="ml-auto" />
+            {/if}
+          </DropdownMenu.Item>
+        {/each}
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
+  </Sidebar.MenuItem>
 </Sidebar.Menu>
